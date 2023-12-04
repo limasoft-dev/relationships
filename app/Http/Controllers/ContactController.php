cc70187e34bf0a237dacecb26139e88b8a84810b
AAndrade <aandrade@limasoft.pt>
2023-12-04 20:25:38 +0000

    create and update user contact info

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        if (Auth::user()->contact) {
+
+            return view('contact.edit', [
+                'contact' => Auth::user()->contact,
+            ]);    // code...
+        } else {
+            return view('contact.create');
+        }
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $dados = $this->validate($request, [
+            'phone' => 'required|digits:9',
+            'address' => 'required|string|max:255',
+        ]);
+
+        auth()->user()->contact()->create($dados);
+
+        return redirect(route('contact.create'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $dados = $this->validate($request, [
+            'phone' => 'required|digits:9',
+            'address' => 'required|string|max:255',
+        ]);
+
+        auth()->user()->contact()->update($dados);
+
+        return redirect(route('contact.create'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/contact/create.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Contact::Create') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="{{route('contact.store')}}" method="post">
+                        @csrf
+                        <div>
+                            <x-input-label for="phone" :value="__('Phone')" />
+                            <x-text-input name="phone" type="text" class="mt-1 block w-full" :value="old('phone')" required autofocus />
+                            <x-input-error class="mt-2" :messages="$errors->get('phone')"/>
+                        </div>
+                        <div>
+                            <x-input-label for="address" :value="__('Address')" />
+                            <x-text-input name="address" type="text" class="mt-1 block w-full" :value="old('address')" required autofocus />
+                            <x-input-error class="mt-2" :messages="$errors->get('address')"/>
+                        </div>
+                        <div class="flex items-center gap-4 mt-5">
+                            <x-primary-button>{{ __('Gravar') }}</x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
