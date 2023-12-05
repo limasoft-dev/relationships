6f846aeda2ec998d731f9efc82154e26abd88999
AAndrade <aandrade@limasoft.pt>
2023-12-05 09:43:28 +0000

    dashboard intents description

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="text-bold text-2xl">
+                        {{ __("Project Demo for Model Relationships!") }}
+                    </div>
+                    <div>
+                        <p class="mt-16">
+                            {{ __("We implements 1:1, 1:N with reverse and N:N without pivot data relationships") }}
+                        </p>
+                        <p>Tecnologies Used:
+                            <ul>
+                                <li>Laravel 10</li>
+                                <li>StarterKit Breeze</li>
+                                <li>Taillwind CSS</li>
+                            </ul>
+                        </p>
+                        <p class=" mt-10">
+                            {{ __("Developed with pedagogic intents by António Andrade")}}
+                        </p>
+
+                        <p class="text-xs">
+                            {{ __("antonio.andrade@ae-salvaterra.pt")}}
+                        </p>
+                    </div>
+
+
+
+
                 </div>
             </div>
         </div>
